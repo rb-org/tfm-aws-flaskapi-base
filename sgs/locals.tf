8bedfc0e7af823099bdb7abd696011312b38b208
Rupert Broad <rupert.broad@gmail.com>
2019-02-02 13:08:36 +0100

    add sgs for alb, r53 hcs

--- sgs/locals.tf
+++ sgs/locals.tf
@@ -1,5 +1,7 @@
 locals {
-  name_prefix        = "${terraform.workspace}"
-  sg_tux_name        = "${local.name_prefix}-tux-sg"
-  sg_db_clients_name = "${local.name_prefix}-db-clients-sg"
+  name_prefix              = "${terraform.workspace}"
+  sg_alb_name              = "${local.name_prefix}-alb-sg"
+  sg_tux_name              = "${local.name_prefix}-tux-sg"
+  sg_db_clients_name       = "${local.name_prefix}-db-clients-sg"
+  sg_r53_health_check_name = "${local.name_prefix}-r53-health-check-sg"
 }
