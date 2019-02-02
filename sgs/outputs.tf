dfff23a2771af3d7c984c82b6e4b0353b8f8e7ec
Rupert Broad <rupert.broad@gmail.com>
2019-02-02 13:27:15 +0100

    add outputs

--- sgs/outputs.tf
+++ sgs/outputs.tf
@@ -5,3 +5,11 @@ output "tux_sg_id" {
 output "db_clients_sg_id" {
   value = "${aws_security_group.sg_db_clients.id}"
 }
+
+output "r53_hcs_sg_id" {
+  value = "${aws_security_group.sg_r53_health_checks.id}"
+}
+
+output "alb_sg_id" {
+  value = "${aws_security_group.sg_alb.id}"
+}
